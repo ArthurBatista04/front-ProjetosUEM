47277913a8b537de712a53d13c617565c2947011
Arthur <arthurbatista04@outlook.com>
2019-10-09 17:07:28 -0300

    dashboard feito

--- src/Components/Admin/Controller/controller.js
+++ src/Components/Admin/Controller/controller.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
 import { Admin, Resource } from 'react-admin';
+import Dashboard from '../View/dashboard';
 import simpleRestProvider from 'ra-data-simple-rest';
 
 export default () => (
-  <Admin dataProvider={simpleRestProvider('http://path.to.my.api')}></Admin>
+  <Admin
+    dataProvider={simpleRestProvider('http://path.to.my.api')}
+    dashboard={Dashboard}
+  ></Admin>
 );
---
+++ src/Components/Admin/View/dashboard.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import { ViewTitle } from 'react-admin';
+export default () => (
+  <Card>
+    <ViewTitle title="Projetos UEM" />
+    <CardContent>Lorem ipsum sic dolor amet...</CardContent>
+  </Card>
+);
