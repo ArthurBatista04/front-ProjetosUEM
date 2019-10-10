11bb39a7a9d8caf003d3c66744b2d2622b96a971
shsandro <shsandro@github.com>
2019-10-10 20:34:06 -0300

    resources projetos

--- src/Components/Admin/Controller/controller.js
+++ src/Components/Admin/Controller/controller.js
@@ -8,6 +8,9 @@ import { DiscenteList } from "../Discente/list";
 import { DocenteList } from "../Docente/list";
 import { AdminList } from "../Administrador/list";
 import { AdminEdit } from "../Administrador/edit";
+import { ProjetosCreate } from "../Projetos/create";
+import { ProjetosEdit } from "../Projetos/edit";
+import { ProjetosList } from "../Projetos/list";
 
 const dataProvider = simpleRestProvider("http://path.to.my.api");
 
@@ -16,5 +19,6 @@ export default () => (
     <Resource name="Discentes" list={DiscenteList} />
     <Resource name="Docentes" list={DocenteList} />
     <Resource name="Admins" list={AdminList} edit={AdminEdit} />
+    <Resource name="Projetos" list={ProjetosList} edit={ProjetosEdit} create={ProjetosCreate} />
   </Admin>
 );
