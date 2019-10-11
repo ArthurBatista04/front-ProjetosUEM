a6996aade57f9c0f2bc3c147ab012dbf36b0bacd
Vinícius Regatieri <viniciusvcr@hotmail.com>
2019-10-10 23:30:34 -0300

    added missing import

--- src/Components/Admin/Projetos/edit.js
+++ src/Components/Admin/Projetos/edit.js
@@ -1,25 +1,38 @@
 import React from "react";
-import { Edit, SimpleForm, TextInput, NumberInput, DateInput, LongTextInput, ReferenceInput, SelectInput } from 'react-admin';
+import {
+  Edit,
+  SimpleForm,
+  TextInput,
+  NumberInput,
+  DateInput,
+  LongTextInput,
+  ReferenceInput,
+  SelectInput,
+  DisabledInput
+} from "react-admin";
 
 export const ProjetosEdit = props => (
   <Edit {...props}>
-    <SimpleForm >
+    <SimpleForm>
       <DisabledInput source="id" />
       <TextInput source="titulo" />
       <ReferenceInput source="docenteId" reference="docentes">
-            <SelectInput label="Orientador" optionText="name" />
+        <SelectInput label="Orientador" optionText="name" />
       </ReferenceInput>
       <ReferenceInput source="docenteId" reference="docentes">
-            <SelectInput label="Coorientador" optionText="name" />
+        <SelectInput label="Coorientador" optionText="name" />
       </ReferenceInput>
       <TextInput source="area" />
       <TextInput source="subarea" />
       <TextInput source="tipo" />
-      <DateInput source="dataInicio"/>
-      <DateInput source="dataTermino"/>
+      <DateInput source="dataInicio" />
+      <DateInput source="dataTermino" />
       <TextInput source="status" />
-      <NumberInput label = "Número de participantes" source="nroParticipantes" />
-      <NumberInput label = "Limite de participantes" source="limiteParticipantes" />
+      <NumberInput label="Número de participantes" source="nroParticipantes" />
+      <NumberInput
+        label="Limite de participantes"
+        source="limiteParticipantes"
+      />
       <LongTextInput source="requisitos" />
       <LongTextInput source="resumo" />
     </SimpleForm>
