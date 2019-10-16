37198b7e71467523c4e13ccd1609d31e74e887e4
Arthur <arthurbatista04@outlook.com>
2019-10-16 15:26:35 -0300

    exclusao de data fim do ps

--- src/Components/Admin/ProcessoSeletivo/View/list.js
+++ src/Components/Admin/ProcessoSeletivo/View/list.js
@@ -5,7 +5,6 @@ export default props => (
   <List {...props}>
     <Datagrid>
       <DateField locales="pt-BR" label="Início" source="dataInicio" />
-      <DateField locales="pt-BR" label="Término" source="dataFim" />
       <BooleanField source="encerrado"></BooleanField>
     </Datagrid>
   </List>
