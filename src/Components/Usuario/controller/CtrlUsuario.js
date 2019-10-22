6ac0de13b672fd1bdda3f4d3bd4717365b7caef1
Vinícius Regatieri <viniciusvcr@hotmail.com>
2019-10-21 23:08:41 -0300

    refactor controller referentes à usuários

--- src/Components/Usuario/controller/CtrlUsuario.js
+++ src/Components/Usuario/controller/CtrlUsuario.js
@@ -4,3 +4,15 @@ import PathName from "../../pathConst.js";
 export const login = async credentials => {
   return axios.post(`${PathName}/api/Usuarios/login?include=user`, credentials);
 };
+
+export const createDocente = docente => {
+  return axios.post(`${PathName}/api/Docentes`, docente);
+};
+
+export const createDiscente = discente => {
+  return axios.post(`${PathName}/api/Discentes`, discente);
+};
+
+export const createUsuario = usuario => {
+  return axios.post(`${PathName}/api/Usuarios`, usuario);
+};
