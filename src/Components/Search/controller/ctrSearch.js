41117dc74cdbfc30ed7ea933426a3a86f350decb
deelefrati <douglasdelefrati@gmail.com>
2019-10-28 17:32:18 -0300

    controlador finalizado

--- src/Components/Search/controller/ctrSearch.js
+++ src/Components/Search/controller/ctrSearch.js
@@ -1,15 +1,20 @@
 import Axios from 'axios';
 import PathName from '../../pathConst';
+import M from 'materialize-css';
 
 export const getAreas = self => {
 	Axios.get(`${PathName}/api/Areas`).then(res => {
 		self.setState({ areas: res.data });
+		let elem = document.getElementById('area');
+		M.FormSelect.init(elem);
 	});
 };
 
 export const getSubareas = self => {
 	Axios.get(`http://localhost:3001/api/Subareas`).then(res => {
 		self.setState({ subareas: res.data });
+		let elem = document.getElementById('subarea');
+		M.FormSelect.init(elem);
 	});
 };
 
@@ -20,7 +25,12 @@ export const handleChange = (self, e) => {
 };
 
 export const handleDatePickerChange = (self, e) => {
+	console.log(e);
 	self.setState({ dataInicio: e.toString() });
+};
+
+export const handleDatePickerChangeTermino = (self, e) => {
+	self.setState({ dataTermino: e.toString() });
 };
 
 export const handleClick = (self, e) => {
@@ -28,36 +38,52 @@ export const handleClick = (self, e) => {
 	const newSearch = {
 		nomeProjeto: self.state.nomeProjeto,
 		nomeOrientador: self.state.nomeOrientador,
-		areas: self.state.areas,
-		subareas: self.state.subareas,
-		tipos: self.state.tipos,
 		optionArea: self.state.optionArea,
 		optionSubarea: self.state.optionSubarea,
-		optionTipo: self.state.tipos
+		optionTipo: self.state.tipos,
+		dataInicio: self.state.dataInicio,
+		dataTermino: self.state.dataTermino
 	};
+
 	const filterSearch = {
-		where: {
-			titulo: self.state.nomeProjeto
-		}
+		where: {},
+		include: []
 	};
-	let resultados = [];
+	if (self.state.nomeProjeto && self.state.nomeProjeto !== '') {
+		filterSearch['where']['titulo'] = self.state.nomeProjeto;
+	}
+	if (self.state.nomeOrientador && self.state.nomeOrientador !== '') {
+		const include = {
+			relation: 'docente',
+			scope: {
+				include: {
+					relation: 'usuario',
+					scope: {
+						where: {
+							nome: self.state.nomeOrientador
+						}
+					}
+				}
+			}
+		};
+		filterSearch.include.push(include);
+	}
+	if (self.state.optionArea && self.state.optionArea !== '') {
+		const include = {
+			relation: 'area'
+		};
+		filterSearch.include.push(include);
+	}
+	if (self.state.optionSubarea && self.state.optionSubarea !== '') {
+		const include = {
+			relation: 'subarea'
+		};
+		filterSearch.include.push(include);
+	}
+
 	Axios.get(`${PathName}/api/Projetos?filter=${JSON.stringify(filterSearch)}`)
 		.then(res => {
-			res.data.forEach(projeto => {
-				const docenteFilter = {
-					where: {
-						id: projeto.usuarioId
-					}
-				};
-				Axios.get(
-					`${PathName}/api/Usuarios?filter=${JSON.stringify(docenteFilter)}`
-				).then(response => {
-					projeto.nomeOrientador = response.data[0].nome;
-					resultados.push(projeto);
-				});
-			});
-			// console.log(resultados);
-			self.setState({ resultadoBusca: resultados });
+			self.setState({ resultadoBusca: res.data });
 			self.setState({ showCards: true });
 		})
 		.catch(err => {
