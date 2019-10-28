35ffd7787b536dadbf5cc5452ac5cf0c2b04d6b6
deelefrati <douglasdelefrati@gmail.com>
2019-10-27 22:44:40 -0300

    create view project

---
+++ src/Components/Projeto/showProject.js
@@ -0,0 +1,116 @@
+import React, { Component } from 'react';
+import Axios from 'axios';
+import PathName from '../pathConst';
+import moment from 'moment';
+import formatDate from '../formatDate';
+import { Link } from 'react-router-dom';
+class ShowProject extends Component {
+	componentWillMount() {
+		Axios.get(`${PathName}/api/Projeots/${this.props.id}`).then(res => {
+			this.setState({ projeto: res.data });
+		});
+	}
+
+	buildRow = (key, value) => (
+		<div className="row">
+			<div className="col s12 m6">
+				<b className="left hide-on-small-and-down">{key}</b>
+				<b className="really-center-span hide-on-med-and-up">{key}</b>
+			</div>
+
+			<div>
+				<span className="right hide-on-small-and-down">{value}</span>
+				<span className="really-center-span hide-on-med-and-up">{value}</span>
+			</div>
+		</div>
+	);
+
+	state = {
+		projeto: [],
+		area: '',
+		subarea: ''
+	};
+
+	render() {
+		const { projeto, area, subarea } = this.state;
+		return (
+			<div className="container">
+				<div className="card-panel z-depth-2">
+					<div className="row">
+						<div id="sobre" className="col s12">
+							<h3 style={titleStyle} className="center">
+								{projeto.titulo}
+							</h3>
+
+							<p className="basicInfo grey-text text-darken-2">
+								{projeto.resumo}
+							</p>
+							<br />
+							<h5>
+								<i className="material-icons left">info</i>Informações
+								importantes
+							</h5>
+							<div className="basicInfo grey-text text-darken-2">
+								<hr className="style-six" />
+								{this.buildRow('Nome do orientador:', projeto.nomeDoOrientador)}
+								{projeto.requisitos
+									? this.buildRow('Tipo do projeto:', projeto.tipo)
+									: null}
+								{this.buildRow(
+									'Inicio do projeto:',
+									formatDate(
+										moment.tz(projeto.dataInicio, 'America/Sao_Paulo').format()
+									)
+								)}
+								<hr className="style-six" />
+
+								{this.buildRow(
+									'Término do projeto:',
+									formatDate(
+										moment.tz(projeto.dataFim, 'America/Sao_Paulo').format()
+									)
+								)}
+								{this.buildRow(
+									'Quantidade máxima de participantes:',
+									projeto.limiteParticipantes
+								)}
+								{this.buildRow(
+									'Quantidade atual de participantes:',
+									projeto.atualParticipantes
+								)}
+								{this.buildRow('Tipo do projeto:', projeto.tipo)}
+								{this.buildRow('Área do projeto:', area)}
+								{this.buildRow('Subárea do projeto:', subarea)}
+							</div>
+							<br />
+							<Link to="/pesquisar" className="btn blue lighten-1 left">
+								Voltar
+							</Link>
+							{this.verifyDate() ? (
+								<Link
+									to={this.getInscrever()}
+									className="btn blue lighten-1 right"
+									style={{
+										textOverflow: 'ellipsis',
+										whiteSpace: 'nowrap',
+										overflow: 'hidden'
+									}}
+								>
+									Inscrever-se
+								</Link>
+							) : null}
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
+
+const titleStyle = {
+	fontWeight: 'bold',
+	fontSize: `${2}rem`,
+	fontFamily: 'Nunito'
+};
+
+export default ShowProject;
