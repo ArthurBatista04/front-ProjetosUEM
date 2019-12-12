0eaccac1e0356746ad3e7f3e1b9bc8ac9e0c90e0
Arthur <arthurbatista04@outlook.com>
2019-12-12 19:06:10 -0300

    fixes

--- src/Components/Admin/ProcessoSeletivo/View/create.js
+++ src/Components/Admin/ProcessoSeletivo/View/create.js
@@ -1,14 +1,14 @@
-import {connect} from 'react-redux';
-import {change, submit, isSubmitting} from 'redux-form';
-import React, {Component, Fragment} from 'react';
+import { connect } from 'react-redux';
+import { change, submit, isSubmitting } from 'redux-form';
+import React, { Component, Fragment } from 'react';
 import {
-	required,
-	Button,
-	SaveButton,
-	CREATE,
-	SimpleForm,
-	withDataProvider,
-	LongTextInput
+  required,
+  Button,
+  SaveButton,
+  CREATE,
+  SimpleForm,
+  withDataProvider,
+  LongTextInput
 } from 'react-admin';
 import IconContentAdd from '@material-ui/icons/Add';
 import IconCancel from '@material-ui/icons/Cancel';
@@ -16,106 +16,106 @@ import Dialog from '@material-ui/core/Dialog';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import PropTypes from 'prop-types';
 import DialogContent from '@material-ui/core/DialogContent';
-// import Br from 'date-fns/locale/pt-BR';
-// import DateFnsUtils from '@date-io/date-fns';
-// import {DateTimeInput} from 'react-admin-date-inputs';
+import Br from 'date-fns/locale/pt-BR';
+import DateFnsUtils from '@date-io/date-fns';
+import { DateTimeInput } from 'react-admin-date-inputs';
 import DialogActions from '@material-ui/core/DialogActions';
-import {HandleProcesso} from '../Controller/CrtlInscrito';
-// DateFnsUtils.prototype.getStartOfMonth = DateFnsUtils.prototype.startOfMonth;
+import { HandleProcesso } from '../Controller/CrtlInscrito';
+DateFnsUtils.prototype.getStartOfMonth = DateFnsUtils.prototype.startOfMonth;
 
 class PostQuickCreateButton extends Component {
-	state = {
-		error: false,
-		showDialog: false
-	};
+  state = {
+    error: false,
+    showDialog: false
+  };
 
-	handleClick = () => {
-		this.setState({showDialog: true});
-	};
+  handleClick = () => {
+    this.setState({ showDialog: true });
+  };
 
-	handleCloseClick = () => {
-		this.setState({showDialog: false});
-	};
+  handleCloseClick = () => {
+    this.setState({ showDialog: false });
+  };
 
-	handleSaveClick = () => {
-		const {submit} = this.props;
+  handleSaveClick = () => {
+    const { submit } = this.props;
 
-		// Trigger a submit of our custom quick create form
-		// This is needed because our modal action buttons are oustide the form
-		submit('post-quick-create');
-	};
+    // Trigger a submit of our custom quick create form
+    // This is needed because our modal action buttons are oustide the form
+    submit('post-quick-create');
+  };
 
-	handleSubmit = values => {
-		HandleProcesso(this, values);
-		this.setState({showDialog: false});
-	};
+  handleSubmit = values => {
+    HandleProcesso(this, values);
+    this.setState({ showDialog: false });
+  };
 
-	render() {
-		const {showDialog} = this.state;
-		const {isSubmitting} = this.props;
+  render() {
+    const { showDialog } = this.state;
+    const { isSubmitting } = this.props;
 
-		return (
-			<Fragment>
-				<Button onClick={this.handleClick} label='Processo seletivo'>
-					<IconContentAdd />
-				</Button>
-				<Dialog
-					fullWidth
-					open={showDialog}
-					onClose={this.handleCloseClick}
-					aria-label='Criar processo seletivo'
-				>
-					<DialogTitle>Processo seletivo</DialogTitle>
-					<DialogContent>
-						<SimpleForm
-							// We override the redux-form name to avoid collision with the react-admin main form
-							form='post-quick-create'
-							resource='turma'
-							// We override the redux-form onSubmit prop to handle the submission ourselves
-							onSubmit={this.handleSubmit}
-							// We want no toolbar at all as we have our modal actions
-							toolbar={null}
-						>
-							<LongTextInput validate={required()} source='prerequisitos' />
-							<LongTextInput validate={required()} source='descricao' />
-							<DateTimeInput
-								label='Início do processo seletivo'
-								source='dataInicio'
-								providerOptions={{utils: DateFnsUtils, locale: Br}}
-							/>
-						</SimpleForm>
-					</DialogContent>
-					<DialogActions>
-						<SaveButton
-							label='Criar processo seletivo'
-							saving={isSubmitting}
-							onClick={this.handleSaveClick}
-						/>
-						<Button label='ra.action.cancel' onClick={this.handleCloseClick}>
-							<IconCancel />
-						</Button>
-					</DialogActions>
-				</Dialog>
-			</Fragment>
-		);
-	}
+    return (
+      <Fragment>
+        <Button onClick={this.handleClick} label="Processo seletivo">
+          <IconContentAdd />
+        </Button>
+        <Dialog
+          fullWidth
+          open={showDialog}
+          onClose={this.handleCloseClick}
+          aria-label="Criar processo seletivo"
+        >
+          <DialogTitle>Processo seletivo</DialogTitle>
+          <DialogContent>
+            <SimpleForm
+              // We override the redux-form name to avoid collision with the react-admin main form
+              form="post-quick-create"
+              resource="turma"
+              // We override the redux-form onSubmit prop to handle the submission ourselves
+              onSubmit={this.handleSubmit}
+              // We want no toolbar at all as we have our modal actions
+              toolbar={null}
+            >
+              <LongTextInput validate={required()} source="prerequisitos" />
+              <LongTextInput validate={required()} source="descricao" />
+              <DateTimeInput
+                label="Início do processo seletivo"
+                source="dataInicio"
+                providerOptions={{ utils: DateFnsUtils, locale: Br }}
+              />
+            </SimpleForm>
+          </DialogContent>
+          <DialogActions>
+            <SaveButton
+              label="Criar processo seletivo"
+              saving={isSubmitting}
+              onClick={this.handleSaveClick}
+            />
+            <Button label="ra.action.cancel" onClick={this.handleCloseClick}>
+              <IconCancel />
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </Fragment>
+    );
+  }
 }
 
 PostQuickCreateButton.propTypes = {
-	dataProvider: PropTypes.func.isRequired,
-	dispatch: PropTypes.func.isRequired,
-	record: PropTypes.object
+  dataProvider: PropTypes.func.isRequired,
+  dispatch: PropTypes.func.isRequired,
+  record: PropTypes.object
 };
 const mapStateToProps = state => ({
-	isSubmitting: isSubmitting('post-quick-create')(state)
+  isSubmitting: isSubmitting('post-quick-create')(state)
 });
 
 const mapDispatchToProps = {
-	change,
+  change,
 
-	submit
+  submit
 };
 export default connect(
-	mapStateToProps,
-	mapDispatchToProps
+  mapStateToProps,
+  mapDispatchToProps
 )(withDataProvider(PostQuickCreateButton));
