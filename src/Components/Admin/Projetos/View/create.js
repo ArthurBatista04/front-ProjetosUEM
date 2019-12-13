caea37785a1b05313e2a44ef746722af9a8f835a
shsandro <shsandro@github.com>
2019-12-13 16:47:44 -0300

    ajuda online de contexto na criação do projeto

--- src/Components/Admin/Projetos/View/create.js
+++ src/Components/Admin/Projetos/View/create.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {
   number,
   required,
@@ -12,15 +12,39 @@ import {
   BooleanInput,
   LongTextInput,
   ReferenceInput
-} from 'react-admin';
+} from "react-admin";
+import Tooltip from "@material-ui/core/Tooltip";
+import Zoom from "@material-ui/core/Zoom";
+
+{
+  /* <Tooltip
+  title={
+    <span style={{ fontSize: "12px" }}>
+      Insira o número de sua matrícula de professor
+    </span>
+  }
+  TransitionComponent={Zoom}
+  enterDelay={500}
+>
+  <TextInput source="titulo" validate={validateTitulo} />
+</Tooltip>; */
+}
 
 const validateTitulo = [required(), minLength(3)];
 const validateLimites = [required(), number()];
-const postDefaultValue = { docenteId: localStorage.getItem('docenteId') };
+const postDefaultValue = { docenteId: localStorage.getItem("docenteId") };
 export default props => (
   <Create {...props}>
     <SimpleForm redirect={false} defaultValue={postDefaultValue}>
-      <TextInput source="titulo" validate={validateTitulo} />
+      <Tooltip
+        title={
+          <span style={{ fontSize: "12px" }}>Insira o título do projeto</span>
+        }
+        TransitionComponent={Zoom}
+        enterDelay={500}
+      >
+        <TextInput source="titulo" validate={validateTitulo} />
+      </Tooltip>
       <ReferenceInput source="coorientadorId" reference="Docentes">
         <SelectInput label="Coorientador" optionText="cargo" />
       </ReferenceInput>
@@ -34,27 +58,107 @@ export default props => (
       >
         <SelectInput label="Subárea" optionText="nome" />
       </ReferenceInput>
-      <TextInput source="tipo" validate={required()} />
-      <DateInput source="dataInicio" validate={required()} />
-      <DateInput source="dataTermino" validate={required()} />
-      <BooleanInput
-        defaultValue={false}
-        label="Projeto em andamento?"
-        source="ativo"
-        validate={required()}
-      />
-      <NumberInput
-        label="Número de participantes"
-        source="atualParticipantes"
-        validate={validateLimites}
-      />
-      <NumberInput
-        label="Limite de participantes"
-        source="limiteParticipantes"
-        validate={number()}
-      />
-      <LongTextInput source="requisitos" />
-      <LongTextInput source="resumo" validate={required()} />
+      <Tooltip
+        title={
+          <span style={{ fontSize: "12px" }}>
+            Insira o tipo do projeto: PIC, PIBIC, PIBIT, ...
+          </span>
+        }
+        TransitionComponent={Zoom}
+        enterDelay={500}
+      >
+        <TextInput source="tipo" validate={required()} />
+      </Tooltip>
+      <Tooltip
+        title={
+          <span style={{ fontSize: "12px" }}>
+            Selecione a data de início do projeto
+          </span>
+        }
+        TransitionComponent={Zoom}
+        enterDelay={500}
+      >
+        <DateInput source="dataInicio" validate={required()} />
+      </Tooltip>
+      <Tooltip
+        title={
+          <span style={{ fontSize: "12px" }}>
+            Selecione a data de término do projeto
+          </span>
+        }
+        TransitionComponent={Zoom}
+        enterDelay={500}
+      >
+        <DateInput source="dataTermino" validate={required()} />
+      </Tooltip>
+      <Tooltip
+        title={
+          <span style={{ fontSize: "12px" }}>
+            Selecione se o projeto já se iniciou
+          </span>
+        }
+        TransitionComponent={Zoom}
+        enterDelay={500}
+      >
+        <BooleanInput
+          defaultValue={false}
+          label="Projeto em andamento?"
+          source="ativo"
+          validate={required()}
+        />
+      </Tooltip>
+      <Tooltip
+        title={
+          <span style={{ fontSize: "12px" }}>
+            Selecione o número atual de participantes do projeto
+          </span>
+        }
+        TransitionComponent={Zoom}
+        enterDelay={500}
+      >
+        <NumberInput
+          label="Número de participantes"
+          source="atualParticipantes"
+          validate={validateLimites}
+        />
+      </Tooltip>
+      <Tooltip
+        title={
+          <span style={{ fontSize: "12px" }}>
+            Selecione o número atual de participantes do projeto
+          </span>
+        }
+        TransitionComponent={Zoom}
+        enterDelay={500}
+      >
+        <NumberInput
+          label="Limite de participantes"
+          source="limiteParticipantes"
+          validate={number()}
+        />
+      </Tooltip>
+      <Tooltip
+        title={
+          <span style={{ fontSize: "12px" }}>
+            Informe os requisitos necessários para os candidatos do projeto
+          </span>
+        }
+        TransitionComponent={Zoom}
+        enterDelay={500}
+      >
+        <LongTextInput source="requisitos" />
+      </Tooltip>
+      <Tooltip
+        title={
+          <span style={{ fontSize: "12px" }}>
+            Informe um resumo sobre o projeto
+          </span>
+        }
+        TransitionComponent={Zoom}
+        enterDelay={500}
+      >
+        <LongTextInput source="resumo" validate={required()} />
+      </Tooltip>
     </SimpleForm>
   </Create>
 );
