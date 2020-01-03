2a946db326065bbac5379065a40902d608307733
Arthur <arthurbatista04@outlook.com>
2020-01-03 14:57:10 -0300

    testes de validacao processo seletivo

--- src/Components/Admin/ProcessoSeletivo/View/edit.js
+++ src/Components/Admin/ProcessoSeletivo/View/edit.js
@@ -11,11 +11,25 @@ import Br from 'date-fns/locale/pt-BR';
 import DateFnsUtils from '@date-io/date-fns';
 import { DateTimeInput } from 'react-admin-date-inputs';
 DateFnsUtils.prototype.getStartOfMonth = DateFnsUtils.prototype.startOfMonth;
+const validateUserCreation = values => {
+  const errors = {};
+  if (!values.prerequsitos) {
+    errors.prerequsitos = ['Pre requisitos são necessários'];
+  }
+  if (!values.descricao) {
+    errors.descricao = ['Uma descrição é necessária'];
+  }
+  const dataAtual = new Date().setHours(0, 0, 0, 0).valueOf();
+  if (values.dataInicio.valueOf() < dataAtual.valueOf()) {
+    errors.dataInicio = ['A data não pode ser anterior ao dia atual!'];
+  }
+  return errors;
+};
 export default props => (
   <Edit actions={false} {...props}>
-    <SimpleForm redirect={false}>
-      <LongTextInput validate={required()} source="prerequisitos" />
-      <LongTextInput validate={required()} source="descricao" />
+    <SimpleForm validate={validateUserCreation} redirect={false}>
+      <LongTextInput source="prerequisitos" />
+      <LongTextInput source="descricao" />
       <DateTimeInput
         label="Início do processo seletivo"
         source="dataInicio"
