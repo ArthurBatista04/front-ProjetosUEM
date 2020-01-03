6bca890b75a9920038254615bf7eff4396fbe104
Arthur <arthurbatista04@outlook.com>
2020-01-03 18:21:17 -0300

    fix no edit

--- src/Components/Admin/ProcessoSeletivo/View/edit.js
+++ src/Components/Admin/ProcessoSeletivo/View/edit.js
@@ -11,25 +11,25 @@ import Br from 'date-fns/locale/pt-BR';
 import DateFnsUtils from '@date-io/date-fns';
 import { DateTimeInput } from 'react-admin-date-inputs';
 DateFnsUtils.prototype.getStartOfMonth = DateFnsUtils.prototype.startOfMonth;
-const validateUserCreation = values => {
+const validarDados = values => {
   const errors = {};
-  if (!values.prerequsitos) {
-    errors.prerequsitos = ['Pre requisitos são necessários'];
-  }
-  if (!values.descricao) {
+  const dataAtual = new Date().setHours(0, 0, 0, 0).valueOf();
+  if (!values.prerequisitos) {
+    errors.prerequisitos = ['Pre requisitos são necessários'];
+  } else if (!values.descricao) {
     errors.descricao = ['Uma descrição é necessária'];
   } else if (values.descricao.length > 200) {
     errors.descricao = ['Max de 200 caracteres'];
-  }
-  const dataAtual = new Date().setHours(0, 0, 0, 0).valueOf();
-  if (values.dataInicio.valueOf() < dataAtual.valueOf()) {
+  } else if (values.dataInicio.valueOf() < dataAtual.valueOf()) {
     errors.dataInicio = ['A data não pode ser anterior ao dia atual!'];
+  } else if (typeof values.encerrado !== 'boolean') {
+    errors.encerrado = ['Encerrado deve ser um valor booleano'];
   }
   return errors;
 };
 export default props => (
   <Edit actions={false} {...props}>
-    <SimpleForm validate={validateUserCreation} redirect={false}>
+    <SimpleForm validate={validarDados} redirect={false}>
       <LongTextInput source="prerequisitos" />
       <LongTextInput source="descricao" />
       <DateTimeInput
