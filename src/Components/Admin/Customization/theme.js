5ccfa4cbd77ba210c799658a76c20eb3e4b3b10c
arthurbatista04 <arthurbatista04@outlook.com>
2020-01-05 01:55:37 -0300

    remove dark mode

--- src/Components/Admin/Customization/theme.js
+++ src/Components/Admin/Customization/theme.js
@@ -1,37 +1,37 @@
-import { createMuiTheme } from '@material-ui/core/styles';
-import indigo from '@material-ui/core/colors/index';
-import red from '@material-ui/core/colors/red';
-import grey from '@material-ui/core/colors/grey';
+import { createMuiTheme } from "@material-ui/core/styles";
+import indigo from "@material-ui/core/colors/index";
+import red from "@material-ui/core/colors/red";
+import grey from "@material-ui/core/colors/grey";
 export default createMuiTheme({
   palette: {
     primary: {
       // light: será calculada com base em palette.primary.main,
-      main: '#00FFFF'
+      main: "#000000"
       // dark: será calculada com base em palette.primary.main,
       // contrastText: será calculada para contrastar com palette.primary.main
     },
     secondary: grey,
     error: red,
-    type: 'dark',
+    type: "light",
     contrastThreshold: 3,
     tonalOffset: 0.2
   },
   typography: {
     // Use the system font instead of the default Roboto font.
     fontFamily: [
-      '-apple-system',
-      'BlinkMacSystemFont',
+      "-apple-system",
+      "BlinkMacSystemFont",
       '"Segoe UI"',
-      'Arial',
-      'sans-serif'
-    ].join(',')
+      "Arial",
+      "sans-serif"
+    ].join(",")
   },
   overrides: {
     MuiButton: {
       // override the styles of all instances of this component
       root: {
         // Name of the rule
-        color: 'white' // Some CSS
+        color: "white" // Some CSS
       }
     }
   }
